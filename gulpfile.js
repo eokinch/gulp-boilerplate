3188eaacc17acd1d31e41ae45e6abb719a687a4d
Emma Kinch <eokinch@gmail.com>
2017-09-01 08:59:56 -0400

    updating for browser sync and autocomplete

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,18 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
 // task to complile sass
 gulp.task('styles', () => {
 	return gulp.src('./dev/styles/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer('last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
 		.pipe(concat('style.css'))
 		.pipe(gulp.dest('./public/styles'))
+		.pipe(reload({stream: true}));
 });
 
 // task to compile JS
@@ -19,12 +24,21 @@ gulp.task('scripts', () => {
 			presets: ['es2015']
 		}))
 		.pipe(gulp.dest('./public/scripts'))
+		.pipe(reload({stream: true}));
+});
+
+// a task for browser-sync
+gulp.task('browser-sync', () => {
+	browserSync.init({
+		server: '.'
+	})
 });
 
 // a task to watch all of the other tasks
 gulp.task('watch', () => {
 	gulp.watch('./dev/styles/**/*.scss', ['styles']);
 	gulp.watch('./dev/scripts/main.js', ['scripts']);
+	gulp.watch('*.html', reload);
 });
 
 // create default task 
